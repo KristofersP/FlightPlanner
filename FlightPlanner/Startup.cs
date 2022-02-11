7e2e85b147588fec5a3ce90f291ab7f74502ca7b
Kristofers Petersons <kristofersp2@gmail.com>
2022-02-11 16:45:43 +0200

    First 6 tests done.

---
+++ FlightPlanner/Startup.cs
@@ -0,0 +1,46 @@
+﻿using FlighPlanner.Handler;
+using Microsoft.AspNetCore.Authentication;
+
+namespace FlighPlanner
+{
+    public static class Startup
+    {
+        public static WebApplication InitializeApp(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
+            var app = builder.Build();
+            Configure(app);
+            return app;
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddAuthentication("BasicAuthentication")
+            .AddScheme<AuthenticationSchemeOptions, BasicAuthenticationHandler>("BasicAuthentication", null);
+
+
+        }
+
+        private static void Configure(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+           app.MapControllers();    
+        }
+    }
+}
