d34191e589d70a9ea094bc4b6dcdc77915c0c64a
Kristofers Petersons <kristofersp2@gmail.com>
2022-02-13 22:25:38 +0200

    Revert "Added a solution"
    This reverts commit ef5c2c13f14f9e3d7481dfe8c1b80b55d74a3ce5.

---
+++ FlightPlanner/Models/AddFlightRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace FlighPlanner.Models
+{
+    public class AddFlightRequest
+    {
+        public Airport From { get; set; }
+
+        public Airport To { get; set;}
+
+        public string Carrier { get; set;}
+
+        public string DepartureTime { get; set;}
+
+        public string ArrivalTime { get; set;}
+    }
+}
